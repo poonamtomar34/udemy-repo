c91fc8e0b7055ec4208ae7db1516e628210f1a63
unknown <poonamsukhdevsinghtomar@mail.com>
2022-03-27 23:30:49 +0530

    random-color-generator

---
+++ javascript-project/random-color-generator/index.js
@@ -0,0 +1,17 @@
+// let colorName=document.querySelector('#colorname')
+// const colorHex='#'+Math.random
+// const colorInString=colorHex.toString
+var colorName=['red','yellow','green','blue','pink','purple'];
+let randomColor=document.querySelector('#colorname');
+let clickBtn=document.querySelector('#btn');
+var changeColor=document.querySelector('body');
+
+clickBtn.addEventListener('click', clickMe);
+function clickMe(){
+    const randomNumber=getRandomNumber();
+    randomColor.innerHTML=colorName[randomNumber];
+    changeColor.style.backgroundColor=colorName[randomNumber];
+}
+function getRandomNumber(){
+    return Math.floor(Math.random()*colorName.length);
+}
