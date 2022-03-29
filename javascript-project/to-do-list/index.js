1d838873d516f78022d484012b5c3b217b5cba3d
unknown <poonamsukhdevsinghtomar@mail.com>
2022-03-29 19:29:21 +0530

    submit to-do-list

--- javascript-project/to-do-list/index.js
+++ javascript-project/to-do-list/index.js
@@ -7,6 +7,7 @@ function clickMe(){
     let getInput=document.querySelector('#inputField');
     let para=document.querySelector('.todos');
     let paragraph=document.createElement("p")
+    //input.value returns any value that is being put in input field
     paragraph.innerText=getInput.value;
     para.appendChild(paragraph);
     inputField.value='';
